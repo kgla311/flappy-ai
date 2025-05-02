4ca08b880d41884b1a7f9712b515ac087b8653f2
kgla311 <125816046+kgla311@users.noreply.github.com>
2025-05-02 19:22:02 +0300

    Add files via upload

---
+++ common.h
@@ -0,0 +1,62 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <random>
+#include <cstdlib> 
+#include <ctime>
+#include <cmath>
+#include <cassert>
+#include <tuple>
+#include <functional>
+#include <fstream>
+#include "SDL.h"
+
+constexpr int    MUTATION_PROBABILITY = 19;
+constexpr int    POPULATION_SIZE = 50;
+constexpr int    TOTAL_HIDDEN_NODES = 10;
+constexpr int    TOTAL_INPUT_NODES = 3;
+constexpr int    TOTAL_OUTPUT_NODES = 2; 
+                 
+constexpr int    SCREEN_HEIGHT = 600;
+constexpr int    SCREEN_WIDTH = 800;
+constexpr double G = 10;
+                
+constexpr int    BIRD_HEIGHT = 50;
+constexpr int    BIRD_WIDTH = 75;
+constexpr int    GAP = 200;
+                        
+
+struct rt //mostly for pipes 
+{
+
+    float x;
+    float y;
+    int w;
+    int h;
+
+    int ticks;
+};
+
+
+ inline double sigmoid(int x) {
+    return 1.f / (1.f + expf(-x)); // reminder for main ai func this one doesnt do anything, now it workin;)
+}
+
+inline int GetRandom(int min, int max) {
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist6(min, max);
+    return dist6(rng); 
+}
+
+inline double GetDT() {
+    static Uint64 LAST = SDL_GetPerformanceCounter();  
+    Uint64 NOW = SDL_GetPerformanceCounter();
+
+    double dt = (double)(NOW - LAST)*1000/ (double)SDL_GetPerformanceFrequency();  // seconds
+    LAST = NOW; 
+
+    return dt;
+}
+
+
---
+++ pipes.cpp
@@ -0,0 +1,54 @@
+#include"pipes.h"
+
+std::vector<rt> Pipe::PIPES;
+
+void Pipe::Draw(int curr, SDL_Renderer* renderer) {
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);  
+    SDL_Rect upper_pipe;
+    upper_pipe.x = static_cast<int>(PIPES[curr].x);
+    upper_pipe.y = static_cast<int>(PIPES[curr].y);
+    upper_pipe.w = PIPES[curr].w;
+    upper_pipe.h = PIPES[curr].h;
+
+    SDL_Rect lower_pipe;
+    lower_pipe.x = static_cast<int>(PIPES[curr].x);
+    lower_pipe.y = PIPES[curr].h + GAP;
+    lower_pipe.w = PIPES[curr].w;
+    lower_pipe.h = SCREEN_HEIGHT - lower_pipe.y;
+
+    SDL_RenderFillRect(renderer, &upper_pipe);
+    SDL_RenderFillRect(renderer, &lower_pipe);
+}
+
+
+void Pipe::Update(int curr, float dt) {
+    PIPES[curr].x -= 1*dt;
+}
+
+bool Pipe::isCollide(int curr, int y) {
+    if ((PIPES[curr].x >= 100 && PIPES[curr].x <= 100 + BIRD_WIDTH) && (PIPES[curr].h >= y || y >= PIPES[curr].h + GAP)) return true; 
+    // 100 is default, 175 with bird hitbox length, 200 - GAP
+    else
+    {
+        return false;
+    }
+}
+
+void Pipe::MapPipes(int amount) {
+    for (int i = 0; i <= amount; i++) {
+        rt temp;
+
+        temp.h = GetRandom(50, 400); // height - gap, 25 so there will be some pipe just for beauty! 
+        temp.w = 50;
+        temp.x = SCREEN_WIDTH - temp.w;
+        temp.y = 0;
+
+        PIPES.push_back(temp);
+    }
+
+}
+
+void Pipe::Reset() {
+    Pipe::PIPES.clear();  
+    Pipe::MapPipes(1000);
+}
